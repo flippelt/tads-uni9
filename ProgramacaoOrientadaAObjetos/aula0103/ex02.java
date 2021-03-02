b6d430b7b2790e0b612f7a646b1e91e5007b2296
Felipe Lippelt <lippelt.f@gmail.com>
2021-03-01 21:37:59 -0300

    Aula  01 de Março

---
+++ ProgramacaoOrientadaAObjetos/aula0103/ex02.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class ex02{
+    public static void main(String[] args) {
+        String nome = "Felipe";
+        JOptionPane.showMessageDialog(null, "Hello, " + nome + "!");
+        System.exit(0);
+    }
+}
