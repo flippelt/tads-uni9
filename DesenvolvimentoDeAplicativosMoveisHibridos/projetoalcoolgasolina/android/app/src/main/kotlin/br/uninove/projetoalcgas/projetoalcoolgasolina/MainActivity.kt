261b54b72908416583cf00a6c9973c90337fced6
Felipe Lippelt <lippelt.f@gmail.com>
2021-04-22 21:58:58 -0300

    Projeto Alcool ou Gasolina em Flutter

---
+++ DesenvolvimentoDeAplicativosMoveisHibridos/projetoalcoolgasolina/android/app/src/main/kotlin/br/uninove/projetoalcgas/projetoalcoolgasolina/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.uninove.projetoalcgas.projetoalcoolgasolina
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ DesenvolvimentoDeAplicativosMoveisHibridos/projetoalcoolgasolina/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Álcool ou Gasolina",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[900],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Icon(
+                Icons.local_gas_station_rounded,
+                size: 160.0,
+                color: Colors.lightBlue[900],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
